08c412605731ae61ef600a2ef4db3b8a06967324
sahaBalaji <67795684+sahaBalaji@users.noreply.github.com>
2023-02-14 10:51:44 +0530

    Add files via upload

---
+++ value.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("How" + "are" + "you?")
+
+	fmt.Println("1+2 =", 1+2)
+	fmt.Println("10.0/2.0 =", 10.0/2.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
